aed7200e7348d482eecdca135a4a1a3f5a4b168c
Kirill Romanov <djaler1@gmail.com>
2021-03-26 11:51:36 +0300

    refactor: move config parts to separated functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 /* eslint-disable global-require */
-import { ServicePlugin } from '@vue/cli-service';
+import { PluginAPI, ServicePlugin } from '@vue/cli-service';
 import browserslist from 'browserslist';
 import { ESBuildMinifyPlugin } from 'esbuild-loader';
+import Config from 'webpack-chain';
 
 const plugin: ServicePlugin = (api) => {
     api.chainWebpack((config) => {
@@ -10,57 +11,69 @@ const plugin: ServicePlugin = (api) => {
 
         const jsRule = config.module.rule('js').test(/\.m?jsx?$/);
 
-        jsRule.uses
-            .delete('thread-loader')
-            .delete('babel-loader');
-
-        jsRule.use('cache-loader')
-            .loader(require.resolve('cache-loader'))
-            .options(api.genCacheConfig('js-esbuild-loader', {
-                target,
-                esbuildLoaderVersion: require('esbuild-loader/package.json'),
-            }));
-
-        jsRule
-            .use('esbuild-loader')
-            .loader('esbuild-loader')
-            .options({
-                target,
-            });
+        configureJs(jsRule, api, target);
 
         const tsRule = config.module.rules.get('ts');
 
         if (tsRule) {
-            tsRule.uses
-                .delete('thread-loader')
-                .delete('babel-loader')
-                .delete('ts-loader');
-
-            tsRule.use('cache-loader')
-                .loader(require.resolve('cache-loader'))
-                .options(api.genCacheConfig('ts-esbuild-loader', {
-                    target,
-                    esbuildLoaderVersion: require('esbuild-loader/package.json'),
-                    // eslint-disable-next-line import/no-extraneous-dependencies
-                    typescriptVersion: require('typescript/package.json'),
-                }, 'tsconfig.json'));
-
-            tsRule
-                .use('esbuild-loader')
-                .loader('esbuild-loader')
-                .options({
-                    target,
-                    loader: 'ts',
-                });
+            configureTs(tsRule, api, target);
         }
 
-        (config.optimization as any).minimizers.delete('terser');
-
-        config.optimization.minimizer('esbuild-minify')
-            .use(ESBuildMinifyPlugin, [{
-                target,
-            }]);
+        configureMinimizer(config, target);
     });
 };
+
+function configureJs(jsRule: Config.Rule<Config.Module>, api: PluginAPI, target: string[]) {
+    jsRule.uses
+        .delete('thread-loader')
+        .delete('babel-loader');
+
+    jsRule.use('cache-loader')
+        .loader(require.resolve('cache-loader'))
+        .options(api.genCacheConfig('js-esbuild-loader', {
+            target,
+            esbuildLoaderVersion: require('esbuild-loader/package.json'),
+        }));
+
+    jsRule
+        .use('esbuild-loader')
+        .loader('esbuild-loader')
+        .options({
+            target,
+        });
+}
+
+function configureTs(tsRule: Config.Rule<Config.Module>, api: PluginAPI, target: string[]) {
+    tsRule.uses
+        .delete('thread-loader')
+        .delete('babel-loader')
+        .delete('ts-loader');
+
+    tsRule.use('cache-loader')
+        .loader(require.resolve('cache-loader'))
+        .options(api.genCacheConfig('ts-esbuild-loader', {
+            target,
+            esbuildLoaderVersion: require('esbuild-loader/package.json'),
+            // eslint-disable-next-line import/no-extraneous-dependencies
+            typescriptVersion: require('typescript/package.json'),
+        }, 'tsconfig.json'));
+
+    tsRule
+        .use('esbuild-loader')
+        .loader('esbuild-loader')
+        .options({
+            target,
+            loader: 'ts',
+        });
+}
+
+function configureMinimizer(config: Config, target: string[]) {
+    (config.optimization as any).minimizers.delete('terser');
+
+    config.optimization.minimizer('esbuild-minify')
+        .use(ESBuildMinifyPlugin, [{
+            target,
+        }]);
+}
 
 export = plugin;
